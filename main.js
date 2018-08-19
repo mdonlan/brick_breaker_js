3d0a64fb210512b8e107dd5831dca5b62d6d96ae
mdonlan8792 <mdonlan8792@yahoo.com>
2018-08-19 19:13:24 -0400

    working on collision detection between ball and paddle

--- main.js
+++ main.js
@@ -4,14 +4,32 @@ let ctx = canvas.getContext("2d");
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
+document.addEventListener("keydown", keyHandler);
+document.addEventListener("keyup", keyHandler);
+
+let ballRadius = 10;
+let ballOffset = ballRadius / 2;
+
+let keys = {
+  a: false,
+  d: false,
+}
+
 let ball = {
   x: 10,
   y: 10,
   velX: 5,
   velY: 5,
-  width: 50,
-  height: 50,
+  width: 10,
+  height: 10,
   radius: 10,
+};
+
+let paddle = {
+  x: canvas.width / 2,
+  y: canvas.height - 30,
+  width: 100,
+  height: 10,
 };
 
 function drawBall() {
@@ -30,8 +48,34 @@ function start() {
 
 function update() {
   clearCanvas();
-  //updateBall();
+  updateBall();
+  updatePaddle();
   requestAnimationFrame(update);
+}
+
+function keyHandler(event) {
+  let key = event.key;
+  if(event.type == "keydown") {
+    keyDown(key);
+  } else if(event.type == "keyup") {
+    keyUp(key);
+  }
+}
+
+function keyDown(key) {
+  if(key == "d") {
+    keys.d = true;
+  } else if(key == "a") {
+    keys.a = true;
+  }
+}
+
+function keyUp(key) {
+  if(key == "d") {
+    keys.d = false;
+  } else if(key == "a") {
+    keys.a = false;
+  }
 }
 
 function updateBall() {
@@ -44,13 +88,65 @@ function updateBall() {
   drawBall();
 }
 
+function updatePaddle() {
+  movePaddle();
+  drawPaddle();
+}
+
+function movePaddle() {
+  if(keys.a) {
+    paddle.x -= 5;
+  }
+  
+  if(keys.d) {
+    paddle.x += 5;
+  }
+}
+
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddle.x, paddle.y, paddle.width, paddle.height);
+  ctx.fillStyle = "Red";
+  ctx.fill();
+}
+
 function moveBall() {
   ball.x += ball.velX;
   ball.y += ball.velY;
 }
 
 function checkCollision() {
+  checkBounds();
+  checkPaddleCollision();
+  
   return false;
+}
+
+function checkPaddleCollision() {
+
+  if (ball.x < paddle.x + paddle.width &&
+   ball.x + ball.width > paddle.x &&
+   ball.y < paddle.y + paddle.height &&
+   ball.y + ball.height > paddle.y) {
+    ballHitPaddle();
+  }
+}
+
+function ballHitPaddle() {
+  ball.velY = -ball.velY;
+}
+
+function checkBounds() {
+  let hitBounds = false;
+  
+  if(ball.x - ballOffset <= 0 || ball.x + ballOffset >= canvas.width) {
+    ball.velX = -ball.velX;
+  }
+  
+  if(ball.y - ballOffset < 0 || ball.y + ballOffset > canvas.height) {
+    ball.velY = -ball.velY;
+  }
+ 
 }
 
 function clearCanvas() {
@@ -58,4 +154,3 @@ function clearCanvas() {
 }
 
 start();
-
