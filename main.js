d52b55030316c1d0ea6f30e1d5ef28adb580b15b
mdonlan8792 <mdonlan8792@yahoo.com>
2018-08-19 20:40:47 -0400

    working on boxes and their collision

--- main.js
+++ main.js
@@ -10,6 +10,17 @@ document.addEventListener("keyup", keyHandler);
 let ballRadius = 10;
 let ballOffset = ballRadius / 2;
 
+let boxesPerRow = 15;
+let boxMargin = 2;
+let boxSpacing = boxesPerRow * boxMargin; 
+console.log(boxSpacing)
+let boxWidthByCanvasSize = (canvas.width - boxSpacing) / boxesPerRow;
+console.log(boxWidthByCanvasSize)
+
+let boxHeight = 30;
+
+let boxes = [];
+
 let keys = {
   a: false,
   d: false,
@@ -36,24 +47,82 @@ function drawBall() {
   
   ctx.beginPath();
   ctx.arc(ball.x, ball.y , ball.radius, 0, 2*Math.PI);
-  ctx.fillStyle = "Red";
+  ctx.fillStyle = "Blue";
   ctx.fill();
   //ctx.stroke();
 }
 
 function start() {
   //drawBall();
+  createBoxes();
   update();
+}
+
+function drawBoxes() {
+  boxes.forEach((box) => {
+    drawBox(box);   
+  });
+}
+
+function drawBox(box) {
+  ctx.beginPath();
+  //ctx.rect(x, y, box.width, box.height);
+  ctx.fillStyle = "Red";
+  ctx.fillRect(box.x, box.y, box.width, box.height);
+}
+
+function createBoxes() {
+  let totalBoxes = 200;
+  let numBoxes = 0;
+  let totalRows = 15;
+  let numRows = 0;
+  let x = 0;
+  let y = boxHeight;
+  while(numRows < totalRows) {
+    x = 0;
+    for(let i = 0; i < boxesPerRow; i++) {
+      createBox(x, y);
+      x += boxWidthByCanvasSize + 2;      
+    }
+    numRows++;
+    y += boxHeight + 2;
+  }
+  
+  /*
+  while(numBoxes < totalBoxes) {
+    drawBox(x, y);
+    x += box.width + 2;
+    if(x + box.width >= canvas.width) {
+      y += box.height + 2;
+      x = box.width;
+    }
+    
+    numBoxes++;
+  }
+  */
+}
+
+function createBox(x, y) {
+  let newBox = {
+    x: x,
+    y: y,
+    height: boxHeight,
+    width: boxWidthByCanvasSize,
+  }
+  boxes.push(newBox)
 }
 
 function update() {
   clearCanvas();
+  
+  drawBoxes();
   updateBall();
   updatePaddle();
   requestAnimationFrame(update);
 }
 
 function keyHandler(event) {
+  //console.log(event.key)
   let key = event.key;
   if(event.type == "keydown") {
     keyDown(key);
@@ -63,17 +132,17 @@ function keyHandler(event) {
 }
 
 function keyDown(key) {
-  if(key == "d") {
+  if(key == "d" || key == "ArrowRight") {
     keys.d = true;
-  } else if(key == "a") {
+  } else if(key == "a" || key == "ArrowLeft") {
     keys.a = true;
   }
 }
 
 function keyUp(key) {
-  if(key == "d") {
+  if(key == "d" || key == "ArrowRight") {
     keys.d = false;
-  } else if(key == "a") {
+  } else if(key == "a" || key == "ArrowLeft") {
     keys.a = false;
   }
 }
@@ -118,8 +187,13 @@ function moveBall() {
 function checkCollision() {
   checkBounds();
   checkPaddleCollision();
+  checkBoxCollision();
   
   return false;
+}
+
+function checkBoxCollision() {
+  
 }
 
 function checkPaddleCollision() {
